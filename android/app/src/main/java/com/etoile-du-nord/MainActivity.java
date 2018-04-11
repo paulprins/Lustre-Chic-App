bce9b7ae9b63b14294615c6cf5052f22119ec48c
Paul Prins <paulprins@gmail.com>
2018-04-11 13:24:36 +0200

    Android app is now working

--- android/app/src/main/java/com/etoile-du-nord/MainActivity.java
+++ android/app/src/main/java/com/etoile-du-nord/MainActivity.java
@@ -1,15 +1,22 @@
 package com.paulandjordan.etoile_du_nord_app;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
-
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
     @Override
-    protected String getMainComponentName() {
-        return "Etoile-du-Nord";
+    protected String getMainComponentName() {  
+		return "Etoile-du-Nord";
+	}
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);
+        super.onCreate(savedInstanceState);
     }
 }
